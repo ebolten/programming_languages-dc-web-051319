1c4f5b22da0c16e91913ce9ee9e3a87b7a596409
Emily Bolten <emilybolten@gmail.com>
2019-05-11 09:19:21 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -47,17 +47,7 @@ def reformat_languages(languages)
   end
 end
 
-def reformat_languages(languages)
-new_languages = {}
-languages.each do |style, language|
-  language.each do |name, attributes|
-  new_languages[name] ||= attributes
-  new_languages[name][:style]=[]
-  new_languages[name][:style]<<style
-end
-end
-new_languages
-end
+
 
 
 
